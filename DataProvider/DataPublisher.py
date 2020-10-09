706691c9c9231384328188e76462d6909ab502c9
lekoook <kokteng1313@gmail.com>
2020-10-10 00:57:24 +0800

    Add ability to publish mock data

--- DataProvider/DataPublisher.py
+++ DataProvider/DataPublisher.py
@@ -2,12 +2,25 @@
 
 import zmq # ZeroMQ
 import time
+import datetime
 import sys
+import logging
+import csv
+import os
 sys.path.append("..")
 import config
 from DataProvider.lib.lsm6ds33 import LSM6DS33
 from DataProvider.lib.lis3mdl import LIS3MDL
 from DataProvider.lib.MedianFilter import MedianFilter
+
+def setupLog():
+    if not os.path.isdir(config.LOG_FOLDER):
+        os.makedirs(config.LOG_FOLDER) 
+
+    now = datetime.datetime.now()
+    logging.basicConfig(
+        filename=config.LOG_FILE_PREFIX 
+        + now.strftime("%d%m%Y_%H%M%S%f"))
 
 def setupPub(pubAddr: str) -> zmq.Socket:
     context = zmq.Context()
@@ -60,9 +73,64 @@ def pubData(publisher: zmq.Socket, topic: str):
         except KeyboardInterrupt:
             break
 
+def pubMock(publisher: zmq.Socket, topic: str, filePath: str):
+    stream = open(filePath, newline='')
+    csvFile = csv.reader(stream, delimiter=',')
+
+    # Median Filters
+    axF = MedianFilter(config.MF_WINDOW_SIZE)
+    ayF = MedianFilter(config.MF_WINDOW_SIZE)
+    azF = MedianFilter(config.MF_WINDOW_SIZE)
+    gxF = MedianFilter(config.MF_WINDOW_SIZE)
+    gyF = MedianFilter(config.MF_WINDOW_SIZE)
+    gzF = MedianFilter(config.MF_WINDOW_SIZE)
+    mxF = MedianFilter(config.MF_WINDOW_SIZE)
+    myF = MedianFilter(config.MF_WINDOW_SIZE)
+    mzF = MedianFilter(config.MF_WINDOW_SIZE)
+
+    next(csvFile) # Skip the first row which is header texts.
+    while True:
+        try:
+            # Read IMU values
+            r = next(csvFile)
+            ax = float(r[1])
+            ay = float(r[2])
+            az = float(r[3])
+            gx = float(r[4])
+            gy = float(r[5])
+            gz = float(r[6])
+            mx = float(r[7])
+            my = float(r[8])
+            mz = float(r[9])
+
+            # Go through median filters
+            ax = int(axF.filt(ax))
+            ay = int(ayF.filt(ay))
+            az = int(azF.filt(az))
+            gx = int(gxF.filt(gx))
+            gy = int(gyF.filt(gy))
+            gz = int(gzF.filt(gz))
+            mx = int(mxF.filt(mx))
+            my = int(myF.filt(my))
+            mz = int(mzF.filt(mz))
+
+            # Publish onto topic
+            publisher.send_string("%s %i %i %i %i %i %i %i %i %i" % (topic, ax, ay, az, gx, gy ,gz, mx, my, mz))
+            print("'%s': %i %i %i %i %i %i %i %i %i" % (topic, ax, ay, az, gx, gy ,gz, mx, my, mz))
+
+            time.sleep(0.020) # 50hz
+        except (KeyboardInterrupt, StopIteration) as e:
+            break
+
 if __name__ == "__main__":
+    setupLog()
     publisher = setupPub(config.DATA_SOCK)
-    pubData(publisher, config.IMU_TOPIC)
+    if config.USE_MOCK_DATA:
+        logging.info("Using MOCK data")
+        pubMock(publisher, config.IMU_TOPIC, config.MOCK_DATA_PATH)
+    else:
+        logging.info("Using REAL data")
+        pubData(publisher, config.IMU_TOPIC)
 
     # Clean up
     context = zmq.Context.instance()
--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 # DataProvider
-DATA_SOCK =    "tcp://127.0.0.1:5556"
-IMU_TOPIC =    "imu"
-MF_WINDOW_SIZE =    3
+DATA_SOCK       = "tcp://127.0.0.1:5556"
+IMU_TOPIC       = "imu"
+MF_WINDOW_SIZE  = 3
+USE_MOCK_DATA   = True # Set to False to read and use actual IMU data.
+MOCK_DATA_PATH  = "mock_data/sample.csv" # Relative to where DataPublisher.py is located.
+LOG_FOLDER      = "logs/"
+LOG_FILE_PREFIX = LOG_FOLDER + "dp_log_"
