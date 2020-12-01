e7ebce6bdb4153674876f526c704c0562330d74d
Glen Goh <32335711+Velucius-Alter@users.noreply.github.com>
2020-12-01 21:18:44 +0800

    Added Mock Publisher for separate feet inputs
    The DualPublisher script reads all the files from within the folder mock_data and publishes the aligned left and right feet sensor data in a format accepted by the DualPredictor script

---
+++ DataProvider/DualPublisher.py
@@ -0,0 +1,152 @@
+#!/usr/bin/python3
+
+import zmq # ZeroMQ
+import time
+import datetime
+import sys
+import logging
+import csv
+import os
+import glob
+import pandas as pd
+sys.path.append("..")
+import config
+from DataProvider.lib.lsm6ds33 import LSM6DS33
+from DataProvider.lib.lis3mdl import LIS3MDL
+from DataProvider.lib.MedianFilter import MedianFilter
+
+def setupLog():
+    if not os.path.isdir(config.LOG_FOLDER):
+        os.makedirs(config.LOG_FOLDER) 
+
+    now = datetime.datetime.now()
+    logging.basicConfig(
+        filename=config.LOG_FILE_PREFIX 
+        + now.strftime("%d%m%Y_%H%M%S%f"))
+
+def setupPub(pubAddr: str) -> zmq.Socket:
+    context = zmq.Context()
+    publisher = context.socket(zmq.PUB)
+    publisher.bind(pubAddr)
+
+    return publisher
+
+def pubData(publisher: zmq.Socket, topic: str):
+    # Create IMU objects
+    accGyro = LSM6DS33()
+    accGyro.enableLSM()
+    mag = LIS3MDL()
+    mag.enableLIS()
+
+    if config.USE_MEDIAN_FILTER:
+        # Median Filters
+        axF = MedianFilter(config.MF_WINDOW_SIZE)
+        ayF = MedianFilter(config.MF_WINDOW_SIZE)
+        azF = MedianFilter(config.MF_WINDOW_SIZE)
+        gxF = MedianFilter(config.MF_WINDOW_SIZE)
+        gyF = MedianFilter(config.MF_WINDOW_SIZE)
+        gzF = MedianFilter(config.MF_WINDOW_SIZE)
+        mxF = MedianFilter(config.MF_WINDOW_SIZE)
+        myF = MedianFilter(config.MF_WINDOW_SIZE)
+        mzF = MedianFilter(config.MF_WINDOW_SIZE)
+
+    while True:
+        try:
+            # Read IMU values
+            ax, ay, az = accGyro.getAccelerometerRaw()
+            gx, gy, gz = accGyro.getGyroscopeRaw()
+            mx, my, mz = mag.getMagnetometerRaw()
+
+            if config.USE_MEDIAN_FILTER:
+                # Go through median filters
+                ax = int(axF.filt(ax))
+                ay = int(ayF.filt(ay))
+                az = int(azF.filt(az))
+                gx = int(gxF.filt(gx))
+                gy = int(gyF.filt(gy))
+                gz = int(gzF.filt(gz))
+                mx = int(mxF.filt(mx))
+                my = int(myF.filt(my))
+                mz = int(mzF.filt(mz))
+
+            # Publish onto topic
+            publisher.send_string("%s %i %i %i %i %i %i %i %i %i" % (topic, ax, ay, az, gx, gy ,gz, mx, my, mz))
+            print("'%s': %i %i %i %i %i %i %i %i %i" % (topic, ax, ay, az, gx, gy ,gz, mx, my, mz))
+
+            time.sleep(0.020) # 50hz
+        except KeyboardInterrupt:
+            break
+
+def pubMock(publisher: zmq.Socket, topic: str, filePath: str):
+
+    left_data = []
+    right_data = []
+    
+    for root, dirs, files in os.walk(config.MOCK_DATA_FOLDER, topdown=False):
+        for folder in dirs:
+            os.chdir('/home/pi/Fog-master/DataProvider/' + os.path.join(root, folder))
+            print(os.getcwd())
+            print("Testing on trial data:", folder)
+    #Getting left and right data readings
+            for file in os.listdir():
+                if 's2.csv' in file:
+                    left_data.append(file)
+                if 's3.csv' in file:
+                    right_data.append(file)
+
+    #combine all files in the list
+            left_csv = pd.concat([pd.read_csv(f, header=None, skiprows = 1) for f in left_data])
+            right_csv = pd.concat([pd.read_csv(f, header=None, skiprows = 1) for f in right_data])
+    #export to csv
+            if os.path.isfile("left_data.csv"):
+                os.remove("left_data.csv")
+            if os.path.isfile("right_data.csv"):
+                os.remove("right_data.csv")            
+                
+            left_csv.to_csv("left_data.csv", index=False, encoding='utf-8-sig', header=None)
+            right_csv.to_csv("right_data.csv", index=False, encoding='utf-8-sig', header=None)
+    #Creating L and R feet data streams        
+            L_stream = open("left_data.csv", newline='')
+            R_stream = open("right_data.csv", newline='')
+            
+            L_csvFile = csv.reader(L_stream, delimiter=',')
+            R_csvFile = csv.reader(R_stream, delimiter=',')
+            
+            while True:
+                try:
+                    # Read IMU values
+                    l = next(L_csvFile)
+                    r = next(R_csvFile)
+                    lax = float(l[4])
+                    lay = float(l[5])
+                    laz = float(l[6])
+                    rax = float(r[4])
+                    ray = float(r[5])
+                    raz = float(r[6])
+                    gt = float(r[10])
+
+                    # Publish onto topic
+                    publisher.send_string("%s %i %i %i %i %i %i %i" % (topic, lax, lay, laz, rax, ray ,raz, gt))
+                    print("%s %i %i %i %i %i %i %i" % (topic, lax, lay, laz, rax, ray ,raz, gt))
+
+                    time.sleep(0.020) # 50hz
+                except (KeyboardInterrupt, StopIteration) as e:
+                    break
+
+            # Clean up
+            os.remove("left_data.csv")
+            os.remove("right_data.csv")
+
+if __name__ == "__main__":
+    setupLog()
+    publisher = setupPub(config.DATA_SOCK)
+    if config.USE_MOCK_DATA:
+        logging.info("Using MOCK data")
+        pubMock(publisher, config.IMU_TOPIC, config.MOCK_DATA_PATHS)
+    else:
+        logging.info("Using REAL data")
+        pubData(publisher, config.IMU_TOPIC)
+
+    # Clean up
+    context = zmq.Context.instance()
+    context.destroy()
